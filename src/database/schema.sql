2bba3918736f624c33cb980f3833367a1a358c6c
Felipe <felipejsilvacosta@gmail.com>
2020-11-11 18:01:55 +1100

    setting the project up

---
+++ src/database/schema.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE find_table;
---
+++ src/server/routes.js
@@ -0,0 +1,19 @@
+// Create the routes for the pages
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+const pool = new Pool({ database: "todo_app" });
+var bodyParser = require('body-parser')
+
+
+app.use(express.static("public"));
+
+app.use(bodyParser.json())
+
+app.get('/', (request, response) => {
+    
+  })
+
+// path to open the index.html in the browser
+// http://localhost:3333/index.html
+app.listen(3333);
