ef2ae12717364ca4d229d42bfec7e961305c4cff
Felipe <felipejsilvacosta@gmail.com>
2020-11-12 12:12:50 +1100

    added google maps API

---
+++ public/script.js
@@ -0,0 +1,12 @@
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.4368, lng: 150.8887 },
+//     zoom: 13,
+//   });
+// }
+
+window.addEventListener('click', (event) => {
+    console.log(event.target)
+})
--- src/server/routes.js
+++ src/server/routes.js
@@ -5,13 +5,10 @@ const { Pool } = require("pg");
 const pool = new Pool({ database: "todo_app" });
 var bodyParser = require('body-parser')
 
-app.use(express.static("public"));
-
+app.use(express.static("public"))
 app.use(bodyParser.json())
 
-app.get('/', (request, response) => {
-    
-  })
+
 
 // path to open the index.html in the browser
 // http://localhost:3333/index.html
