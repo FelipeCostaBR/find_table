854e7b28f82ba40bfc6633b1f59177d88daaa241
Felipe <felipejsilvacosta@gmail.com>
2020-11-14 15:52:05 +1100

    add infoWindow and restaurant list - restaurant_map

---
+++ public/restaurant_map.js
@@ -0,0 +1,122 @@
+let RestaurantListContainer = document.querySelector('.restaurant-list-container')
+
+let map;
+let iconImage = "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+
+function initMap() {
+  let options = {
+    center: { lat: -12.7502038, lng: 14.8621315 }, // set a default
+    zoom: 13,
+  };
+  map = new google.maps.Map(document.getElementById("map"), options);
+
+  main();
+
+}
+
+function addMarker(props) {
+      var marker = new google.maps.Marker({
+      position: props.coords,
+      map: props.map,
+    });
+
+    //check for customIcon
+    if (props.iconImage) {
+      marker.setIcon(props.iconImage)
+    }
+
+    // check name
+    if(props.name) {
+     var infoWindow = new google.maps.InfoWindow({
+        content: props.name
+      })
+      marker.addListener('click', () => {
+        infoWindow.open(props.map, marker)
+      })
+    }
+}
+
+function getPosition() {
+  // Simple wrapper
+  return new Promise((res, rej) => {
+    navigator.geolocation.getCurrentPosition(res, rej, {enableHighAccuracy: true});
+  });
+}
+
+
+function createRestaurantList(restaurant) {
+  let restaurantList = document.createElement("div")
+  restaurantList.className = 'restaurant-list'
+  RestaurantListContainer.appendChild(restaurantList)
+
+  let name = document.createElement("p")
+  name.className= 'restaurant-name'
+  name.innerHTML = `Name: ${restaurant.name}`
+  
+
+  let status = document.createElement("p")
+  status.className= 'restaurant-status'
+  status.innerHTML = `Status: ${restaurant.opening_hours.open_now}`
+
+  let rating = document.createElement("p")
+  rating.className= 'restaurant-rating'
+  rating.innerHTML = `Rating: ${restaurant.rating}` 
+
+  let vicinity = document.createElement("p")
+  vicinity.className= 'restaurant-rating'
+  vicinity.innerHTML = `Address: ${restaurant.vicinity}`
+
+  restaurantList.appendChild(name)
+  restaurantList.appendChild(status)
+  restaurantList.appendChild(rating)
+  restaurantList.appendChild(vicinity)
+}
+
+// wait for getPosition to complete
+function main() {
+  getPosition().then((res) => {
+
+    const { latitude, longitude } = res.coords;
+    // change the map location to the current user using browser GPS
+    map.setCenter({ lat: latitude, lng: longitude });
+    // add marker for the user location
+    addMarker(
+      {coords: {lat: latitude, lng: longitude }, map}
+      );
+
+    axios
+      .post("/api/user_location", {
+        latitude: latitude,
+        longitude: longitude,
+      })
+      .then((response) => {
+        // response with all the restaurants
+        response.data.results.forEach((restaurant) => {
+          let RestaurantCoors = restaurant.geometry.location;
+          // add a restaurants marker
+          addMarker(
+            {coords: RestaurantCoors, map, iconImage, name: `<h2>${restaurant.name}</h2><button>➡️</button>`}
+            );
+            // Add Restaurant list
+            createRestaurantList(restaurant)
+        });
+      });
+  });
+}
+
+
+// business_status: "OPERATIONAL"
+// geometry: {location: {…}, viewport: {…}}
+// icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png"
+// name: "RocoMamas"
+// opening_hours: {open_now: true}
+// photos: [{…}]
+// place_id: "ChIJkQGqdRZp1moRcr3nAEBbLAA"
+// plus_code: {compound_code: "6VGQ+PP Maribyrnong, Victoria", global_code: "4RJ66VGQ+PP"}
+// price_level: 2
+// rating: 4.5
+// reference: "ChIJkQGqdRZp1moRcr3nAEBbLAA"
+// scope: "GOOGLE"
+// types: (5) ["meal_takeaway", "restaurant", "food", "point_of_interest", "establishment"]
+// user_ratings_total: 223
+// vicinity: "Level 1 Shop 1103/120-200 Rosamond Rd, Maribyrnong"
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,10 +1,9 @@
 // Create the routes for the pages
 const express = require("express");
 const app = express();
-const { Pool } = require("pg");
-const pool = new Pool({ database: "todo_app" });
+// const { Pool } = require("pg");
+// const pool = new Pool({ database: "todo_app" });
 const restaurantsLocation = require('./controllers/restaurant_controller')
-const api_key = process.env.GOOGLE_MAPS_API_KEY;
 let bodyParser = require('body-parser')
 
 
@@ -15,25 +14,27 @@ app.set('views', './public')
 app.set('view engine', 'ejs')
 
 
-
-app.post('/api/user_location',(request, res) => {
+app.post('/api/user_location',(request, response) => {
     const { latitude, longitude } = request.body
     // HTTP request to find restaurants location
     // parameters is user current location
-    restaurantsLocation(latitude,longitude).then(response => {
+    restaurantsLocation(latitude,longitude).then(res => {
         // add restaurants tables, use place_id as key
-        res.json(response.data)
+        response.json(res.data)
     })
 })
-
-app.get('/restaurant', (request, response) => {
-    // rendering templates
-    response.render('restaurant_map', {api_key: api_key})
-});
 
 app.get('/restaurant/details', (request, response) => {
     response.render('restaurant_details')
 })
+
+
+// app.get('/restaurant', (request, response) => {
+//     // rendering templates
+//     response.render('restaurant_map')
+// });
+
+
 
 // path to open the index.html in the browser
 // http://localhost:3333/index.html
