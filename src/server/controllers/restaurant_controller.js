f2e46876a69a69c9a92729de347eafa71907211f
Bern <bernnystrom@gmail.com>
2020-11-15 18:03:22 +0800

    Test search results to return more than 20 in API request

--- src/server/controllers/restaurant_controller.js
+++ src/server/controllers/restaurant_controller.js
@@ -3,6 +3,8 @@ const api_key = process.env.GOOGLE_MAPS_API_KEY;
 function restaurantsLocation(userLat, userLog) {
   return axios.get(
     `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat},${userLog}&radius=6000&type=restaurant&keyword=burger&key=${api_key}`
+  ).then(
+    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${api_key}&pagetoken=next_page_token`
   )
 }
 
