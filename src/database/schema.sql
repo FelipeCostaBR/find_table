ef9ea3ecd786c60161b30e9e18ab6e9dddc52627
sannanijaz <57088076+Sannan-Ijaz@users.noreply.github.com>
2020-11-16 19:33:00 +0500

    restaurant

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -3,3 +3,4 @@ CREATE DATABASE project3;
 CREATE TABLE restaurantinfo (id INTEGER NOT NULL, name TEXT, geometry NUMERIC[],opening_hours TEXT, place_id TEXT, rating TEXT, types TEXT[], photosurl TEXT, tablereserve REAL, bookingtime INTEGER[]);
 
 INSERT into restaurantinfo(name, geometry, opening_hours, place_id, rating, types, tablereserve, bookingtime) VALUES ('CRUISE BAR', '{-33.86,151.2}', 'true', 'ChIJi6C1MxquEmsR9-c-3O48ykI', '4','{bar,restaurant,food,point_of_interest,establishment}', '1', '{8,9}');
+
