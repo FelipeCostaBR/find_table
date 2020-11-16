100a38d600eb7db94e673d67cbde2f8dbb1457f5
Felipe <felipejsilvacosta@gmail.com>
2020-11-16 16:30:10 +1100

    change the routes.js path

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join( 'public'))); 
+app.use(express.static(path.join(__dirname,'/public'))); 
 
 app.set('views', './public')
 app.set('view engine', 'ejs')
